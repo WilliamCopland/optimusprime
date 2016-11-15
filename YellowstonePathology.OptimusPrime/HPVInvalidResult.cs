7597763202131d2a00041b08a04bdf1b21119673
SidHarder <softwarenavigator@gmail.com>
2016-11-15 07:45:23 -0700

    initial commit

---
+++ YellowstonePathology.OptimusPrime/HPVInvalidResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YellowstonePathology.OptimusPrime
+{
+    public class HPVInvalidResult : HPVResult
+    {
+        public HPVInvalidResult()
+        {            
+            this.m_ResultCode = "HPVNVLD";
+            this.m_Result = HPVResult.InvalidResult;
+        }
+    }
+}
